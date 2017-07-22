965714933b616a6d3e9460f2ecfc309a9f2538ff
1626907 <1626907@gmail.com>
2017-07-22 23:22:32 +0800

    test

--- tribus.c
+++ tribus.c
@@ -8,7 +8,7 @@
 #include "sha3/sph_keccak.h"
 #include "sha3/sph_echo.h"
 
-#define _ALIGN(x) __attribute__ ((aligned(x)))
+#include "common.h"
 
 void tribus_hash(const char* input, char* output, uint32_t len)
 {
